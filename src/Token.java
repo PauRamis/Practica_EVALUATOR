b6c4108f2495041b8961878159314bdd07ca9da1
Pau <pauramisnunez@gmail.com>
2023-04-21 17:17:18 +0200

    Initial commit

---
+++ src/Token.java
@@ -0,0 +1,73 @@
+public class Token {
+    enum Toktype {
+        NUMBER, OP, PAREN
+    }
+
+    // Pensa a implementar els "getters" d'aquests atributs
+    private Toktype ttype;
+    private int value;
+    private char tk;
+
+    public Toktype getTtype() {
+        return ttype;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public char getTk() {
+        return tk;
+    }
+
+    // Constructor privat. Evita que es puguin construir objectes Token externament
+    private Token() {
+    }
+
+    // Torna un token de tipus "NUMBER"
+    static Token tokNumber(int value) {
+        Token t = new Token();
+        t.ttype = Toktype.NUMBER;
+        t.value = value;
+        return t;
+    }
+
+    // Torna un token de tipus "OP"
+    static Token tokOp(char c) {
+        Token t = new Token();
+        t.ttype = Toktype.OP;
+        t.tk = c;
+        return t;
+    }
+
+    // Torna un token de tipus "PAREN"
+    static Token tokParen(char c) {
+        Token t = new Token();
+        t.ttype = Toktype.PAREN;
+        t.tk = c;
+        return t;
+    }
+
+    // Mostra un token (conversió a String)
+    public String toString() {
+        return "";
+    }
+
+    // Mètode equals. Comprova si dos objectes Token són iguals
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    // A partir d'un String, torna una llista de tokens
+    public static Token[] getTokens(String expr) {
+        String[] charList = expr.split(" ");
+        Token[] tokenList = new Token[expr.length()];
+
+        for (int i = 0; i < expr.length(); i++) {
+            String  c = charList[i];
+
+        }
+
+        return tokenList;
+    }
+}
