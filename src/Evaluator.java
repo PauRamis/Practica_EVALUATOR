8a9fcc2685062992e5d339f128846be4e05627c6
Pau <pauramisnunez@gmail.com>
2023-04-24 16:50:45 +0200

    Start Evaluator

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 public class Evaluator {
 
@@ -5,15 +6,27 @@ public class Evaluator {
         // Convertim l'string d'entrada en una llista de tokens
         Token[] tokens = Token.getTokens(expr);
         // Efectua el procediment per convertir la llista de tokens en notació RPN
+        Token[] rpn = convertRPN(tokens);
         // Finalment, crida a calcRPN amb la nova llista de tokens i torna el resultat
-        return 0;
+        return calcRPN(rpn);
+    }
+
+    private static Token[] convertRPN(Token[] tokens) {
+        Stack<Token> operatorStack = new Stack<>();
+        Token[] output = new Token[tokens.length];
+
+        //FIND THE RIGHT PATTERN!!!
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].getTtype() == Token.Toktype.NUMBER)
+                output[i] = tokens[i];
+            else operatorStack.add(tokens[i]);
+        }
+
+        return output;
     }
 
     public static int calcRPN(Token[] list) {
         // Calcula el valor resultant d'avaluar la llista de tokens
         return 0;
-        
     }
-
-
 }
